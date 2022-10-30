3732b64a745b700534bd48e07c81700e4a54da7c
Kyle Marek <kmarek@pdinc.us>
2022-10-30 16:01:16 -0400

    Implement SV-230315r627750_rule

---
+++ rules/SV-230315r627750_rule/impl.bash
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+shopt -s inherit_errexit
+
+exec 0</dev/null
+
+source "$ACCIDENT_STIG_IMPL_BASH_LIB_DIR/accident-stig-library.bash"
+
+
+#
+# Group ID (Vulid):       V-230315
+#
+# Group Title:            SRG-OS-000480-GPOS-00227
+#
+# Rule ID:                SV-230315r627750_rule
+#
+# Severity:               CAT II
+#
+# Rule Version (STIG-ID): RHEL-08-010675
+#
+# Rule Title:             RHEL 8 must disable core dump backtraces.
+#
+
+
+config_file="50-accident-stig-disable-coredump-backtraces.conf"
+
+reboot_file="$ACCIDENT_STIG_IMPL_RUN_DIR/needs_reboot"
+
+# check for pending reboot from previous run
+need_reboot=0
+if [ -e "$reboot_file" ]; then
+	need_reboot=1
+fi
+
+# if pending reboot, assume compliant
+if [ "$need_reboot" -ne 0 ]; then
+	compliance="$ACCIDENT_CONST_COMPLIANT"
+
+# if no pending reboot, check compliance now
+else
+
+	if [ -e "/etc/systemd/coredump.conf.d/$config_file" ]; then
+		compliance="$ACCIDENT_CONST_COMPLIANT"
+	else
+		compliance="$ACCIDENT_CONST_NONCOMPLIANT"
+	fi
+
+	# if allowed, make compliant
+
+	if [ "$compliance" = "$ACCIDENT_CONST_NONCOMPLIANT" -a "$ACCIDENT_STIG_IMPL_ALLOW_IMPLEMENT" -ne 0 ]; then
+
+		if [ ! -e "/etc/systemd/coredump.conf.d/$config_file" ]; then
+
+			# make compliant
+
+			if [ ! -d "/etc/systemd/coredump.conf.d" ]; then
+				trace mkdir "/etc/systemd/coredump.conf.d"
+			fi
+
+			trace cp -nv "$ACCIDENT_STIG_IMPL_RULE_DIR_RES/$config_file" -T "/etc/systemd/coredump.conf.d/$config_file"
+
+			# assume compliant until next reboot
+			compliance="$ACCIDENT_CONST_COMPLIANT"
+			touch "$reboot_file"
+			need_reboot=1
+
+		else
+			warning "Target config file already exists. Did someone override the setting?"
+		fi
+	fi
+fi
+
+if [ "$need_reboot" -ne 0 ]; then
+	warning "A reboot is required to apply new config files."
+fi
+
+compliance --rule-id "SV-230315r627750_rule" --status "$compliance"
+
+
+exit 0
